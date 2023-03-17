61e239fec92cef58c9745f21d26e533e0c0eb11e
Mortey Kafui <morteykafui@gmail.com>
2023-03-16 18:23:02 -0700

    add remove and clear functions

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
 
 const addItem = e => {
   e.preventDefault();
@@ -37,4 +38,18 @@ const createIcon = classes => {
   return icon;
 };
 
+const removeItem = e => {
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    e.target.parentElement.parentElement.remove();
+  }
+};
+
+const clearItems = () => {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+};
+
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItems);
